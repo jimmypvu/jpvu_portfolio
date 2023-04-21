a85123f6de98ca8e53f0aa5f01651c142c267d6b
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-04-21 16:53:41 -0700

    form handler test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//nav toggle
 let navToggle = document.querySelector('.nav-toggle')
 let navLinks = document.querySelectorAll('.nav__link')
 
@@ -17,3 +18,22 @@ document.addEventListener('keydown', function(e){
         }
     }
 )
+
+//phone input
+let phoneInput = document.querySelector("#phone");
+window.intlTelInput(phoneInput, {
+  separateDialCode: true
+});
+
+//form submit
+// function sendEmail(){Email.send({
+//     Host : "smtp.gmail.com",
+//     Username : "jimmyphuvu@gmail.com",
+//     Password : "abcDd6969$$",
+//     To : "jimmyphuvu@gmail.com",
+//     From : document.querySelector("#email").value,
+//     Subject : "New Contact Form Submission",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );}
