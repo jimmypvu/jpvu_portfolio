44ad9eb053b2852a9c5f537faedd25f81187aa7b
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-04-22 00:23:25 -0700

    updated section content and added form validation

--- js/main.js
+++ js/main.js
@@ -19,8 +19,27 @@ document.addEventListener('keydown', function(e){
     }
 )
 
-//phone input
+//twilio phone input init
 let phoneInput = document.querySelector("#phone");
 window.intlTelInput(phoneInput, {
   separateDialCode: true
 });
+
+//form validation
+let contactForm = document.querySelector(".contact_form");
+contactForm.addEventListener("submit", validateForm);
+
+function validateForm(emailInputText){
+    let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // let nameFormat = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+    // let phoneFormat = /^(\+?1)?[-.\s]?\(?[2-9]\d{2}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+
+    if(emailInputText.value.match(emailFormat)){
+        return true;
+    }else{
+        alert("Invalid email address! Please double-check your email address");
+        document.contact_form.email.focus();
+        event.preventDefault();
+        return false;
+    }
+}
